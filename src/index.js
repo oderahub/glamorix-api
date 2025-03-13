9f602928d2985930eca1ef2a671cb5dd406b0a82
kemsguy7 <mattidungafa@gmail.com>
2025-03-13 04:51:02 +0100

    updated package.json to fix node start issue

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
-import dotenv from 'dotenv'
-import app from './app.js'
-import sequelize from './config/database.js'
-import logger from './utils/logger.js'
+import dotenv from 'dotenv';
+import app from './app.js';
+import sequelize from './config/database.js';
+import logger from './utils/logger.js';
 import https from 'https';
 import cron from 'node-cron';
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-    try {
-        await sequelize.sync()
-        app.listen(PORT, () => {
-            logger.info(`Server is running on port ${PORT}`)
-        })
-    } catch (error) {
-        logger.error('Failed to start server due to database connection error:', error)
-        process.exit(1)
-    }
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    logger.error('Failed to start server due to database connection error:', error);
+    process.exit(1);
+  }
 }
 
 function keepAlive(url) {
-    https
-        .get(url, (res) => {
-            logger.info(`Status: ${res.statusCode}`);
-        })
-        .on('error', (error) => {
-            console.error(`Error: ${error.message}`);
-        });
+  https
+    .get(url, (res) => {
+      logger.info(`Status: ${res.statusCode}`);
+    })
+    .on('error', (error) => {
+      console.error(`Error: ${error.message}`);
+    });
 }
 
 // Schedule a job to keep the server alive
 cron.schedule('*/5 * * * *', () => {
-    keepAlive('');
-    logger.info('Pinged the server every 5 minutes');
+  keepAlive('https://omorix-backend.onrender.com');
+  logger.info('Pinged the server every 5 minutes');
 });
 
-
-startServer()
+startServer();
