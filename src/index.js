59c877d4e9e0cdb3940f24b2d4fd723799926c97
oderahub <okere_chidera@yahoo.com>
2025-03-26 01:24:18 +0100

    feat: addad get orders for customer

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 async function startServer() {
     try {
-        await sequelize.sync({ alter: true });
+        await sequelize.sync();
         app.listen(PORT, '0.0.0.0', () => {
             logger.info(`Server is running on port ${PORT}`);
         });
